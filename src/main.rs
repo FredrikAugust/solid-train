e4669914e57ddf0eaf6fbbfcaf6a97317cb55f1d
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2022-10-31 21:30:12 +0100

    hm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#[derive(Debug)]
+struct Post<'a> {
+    id: &'a str,
+    name: &'a str,
+    allocated: f32,
+}
+
+#[derive(Debug)]
+struct YearMonth(i16, i8);
+
+#[derive(Debug)]
+struct Budget<'a> {
+    posts: Vec<Post<'a>>,
+    date: &'a YearMonth,
+}
+
+#[derive(Debug)]
+struct Movement<'a> {
+    amount: f32,
+    payee: &'a str,
+    timestamp: &'a str,
+    budget_id: &'a str,
+}
+
+#[derive(Debug)]
+struct State<'a> {
+    budgets: Vec<Budget<'a>>,
+    movements: Vec<Budget<'a>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test_data: State = State {
+        budgets: vec![Budget {
+            posts: vec![],
+            date: &YearMonth(2022, 10),
+        }],
+        movements: vec![],
+    };
+
+    println!("{:?}", test_data);
 }
