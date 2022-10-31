5a0d192b1279665eff1c43d4111bdb5997d9412a
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2022-10-31 22:51:56 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Post<'a> {
     id: &'a str,
     name: &'a str,
     allocated: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Movement<'a> {
     amount: f32,
     payee: &'a str,
@@ -15,25 +15,46 @@ struct Movement<'a> {
 
 #[derive(Debug)]
 struct Budget<'a> {
-    posts: Vec<&'a Post<'a>>,
-    movements: Vec<&'a Movement<'a>>,
+    posts: Vec<Post<'a>>,
+    movements: Vec<Movement<'a>>,
 }
 
-impl Budget<'_> {
-    fn money_left_in_post(self, post_id: &str) -> f32 {
+impl<'a> Budget<'a> {
+    fn remaining_in_post(&'a self, post_id: &'a str) -> f32 {
         let deductions = self
             .movements
-            .into_iter()
+            .iter()
             .filter(|movement| movement.post_id == post_id)
             .fold(0.0f32, |accum, movement| accum + movement.amount);
 
-        return self
+        let allocated_in_post = self
             .posts
-            .into_iter()
+            .iter()
             .find(|post| post.id == post_id)
             .unwrap()
-            .allocated
-            - deductions;
+            .allocated;
+
+        return allocated_in_post - deductions;
+    }
+
+    fn add_movement(&mut self, movement: Movement<'a>) {
+        self.movements.push(movement);
+    }
+
+    fn print_movements(&self) {
+        self.movements.iter().for_each(|movement| {
+            let post = self.posts.iter().find(|post| post.id == movement.post_id);
+
+            let post_name = match post {
+                Some(post_ref) => post_ref.name,
+                None => "Unknown",
+            };
+
+            println!(
+                "[{:<22}] {:<15} {}: {}",
+                movement.timestamp, movement.payee, post_name, movement.amount
+            )
+        })
     }
 }
 
@@ -51,11 +72,21 @@ fn main() {
         post_id: &test_post.id,
     };
 
-    let test_data: Budget = Budget {
-        movements: vec![&test_movement],
-        posts: vec![&test_post],
+    let mut test_data: Budget = Budget {
+        movements: vec![test_movement.clone()],
+        posts: vec![test_post.clone()],
     };
 
-    println!("{:?}", test_data);
-    println!("{:?}", test_data.money_left_in_post(&test_post.id));
+    println!("{:?}", &test_data);
+    println!("{:?}", &test_data.remaining_in_post(&test_post.id));
+
+    test_data.add_movement(Movement {
+        amount: 25.20,
+        payee: "Kiwi",
+        post_id: "groceries",
+        timestamp: "2022-10-31T16:40:40",
+    });
+
+    println!("{:?}", test_data.remaining_in_post(&test_post.id));
+    test_data.print_movements();
 }
